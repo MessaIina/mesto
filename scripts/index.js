494d8db5286827d91e3a8f63c41fd375c5114aac
MessaIina <121053491+MessaIina@users.noreply.github.com>
2023-04-11 15:32:23 +0700

    2 rew 6PR

--- scripts/index.js
+++ scripts/index.js
@@ -1,115 +1,116 @@
-const popups = document.querySelectorAll('.popup');
+const popups = document.querySelectorAll('.popup'); 
 const closeBtns = document.querySelectorAll('.popup__close-btn');
 const editBtn = document.querySelector('.profile__edit-btn');
 const profilePopup = document.querySelector('.profile-popup'); 
 const editForm = document.forms['edit-profile-form'];
 const nameInput = editForm.querySelector('#name');
-const jobInput = editForm.querySelector('#job'); 
-const profileName = document.querySelector('.profile__name'); 
-const profileJob = document.querySelector('.profile__about');
-const addBtn = document.querySelector('.profile__add-btn');
-const cardPopup = document.querySelector('.card-popup');
-const addForm = document.forms['add-card-form'];
-const cardName = addForm.querySelector('#place');
-const cardLink = addForm.querySelector('#link');
-const imagePopup = document.querySelector('.image-popup');
-const cardImage = imagePopup.querySelector('.card-zoom__image');
-const cardCaption = imagePopup.querySelector('.card-zoom__caption');
-const cardTemplate = document.querySelector('#card-template').content;
+const jobInput = editForm.querySelector('#job');  
+const profileName = document.querySelector('.profile__name');  
+const profileJob = document.querySelector('.profile__about'); 
+const addBtn = document.querySelector('.profile__add-btn'); 
+const cardPopup = document.querySelector('.card-popup'); 
+const addForm = document.forms['add-card-form']; 
+const cardName = addForm.querySelector('#place'); 
+const cardLink = addForm.querySelector('#link'); 
+const imagePopup = document.querySelector('.image-popup'); 
+const cardImage = imagePopup.querySelector('.card-zoom__image'); 
+const cardCaption = imagePopup.querySelector('.card-zoom__caption'); 
+const cardTemplate = document.querySelector('#card-template').content; 
 const cardsList = document.querySelector('.cards__list');
+ 
+
 
 function closePopupWhenPressEsc(evt) {
-	if (evt.key === 'Escape') {
-	  const openedPopup = document.querySelector('.popup_opened');
-	  closePopup(openedPopup);
-	}
-  }
-
-function openPopup(popup) { 
-	document.addEventListener('keydown', closePopupWhenPressEsc);
-	popup.classList.add('popup_opened'); 
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_opened');
+        closePopup(openedPopup);
+    }
 }
-  
-function closePopup(popup) { 
-	document.removeEventListener('keydown', closePopupWhenPressEsc);
-	popup.classList.remove('popup_opened');
+
+function openPopup(popup) {
+    document.addEventListener('keydown', closePopupWhenPressEsc);
+    popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+    document.removeEventListener('keydown', closePopupWhenPressEsc);
+    popup.classList.remove('popup_opened');
 }
 
 popups.forEach((popup) =>
-  popup.addEventListener('mousedown', (evt) => {
-    if (
-      evt.target.classList.contains('popup') ||
-      evt.target.classList.contains('popup__close-btn')
-    ) {
-      closePopup(popup);
-    }
-  })
+    popup.addEventListener('mousedown', (evt) => {
+        if (evt.target.classList.contains('popup')) {
+            closePopup(popup);
+        }
+    })
 );
 
 function setLikeListener(card) {
-	const cardLike = card.querySelector('.card__like-btn');
-	cardLike.addEventListener('click', () => {
-		cardLike.classList.toggle('card__like-btn_liked');
-	});
+    const cardLike = card.querySelector('.card__like-btn');
+    cardLike.addEventListener('click', () => {
+        cardLike.classList.toggle('card__like-btn_liked');
+    });
 }
 
 function setDeleteListener(card) {
-	const deleteCard = card.querySelector('.card__delete-btn');
-	deleteCard.addEventListener('click', () => {
-		card.remove();
-	});
+    const deleteCard = card.querySelector('.card__delete-btn');
+    deleteCard.addEventListener('click', () => {
+        card.remove();
+    });
 }
 
 function handleCardClick(item) {
-	openPopup(imagePopup);
-	cardImage.src = item.link;
-	cardImage.alt = item.name;
-	cardCaption.textContent = item.name;
+    openPopup(imagePopup);
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardCaption.textContent = item.name;
 }
 
 closeBtns.forEach(function (btn) {
-	const popup = btn.closest('.popup');
-	btn.addEventListener('click', () => closePopup(popup));
+    const popup = btn.closest('.popup');
+    btn.addEventListener('click', () => closePopup(popup));
 });
 
-function createCard (item) {
-	const card = cardTemplate.querySelector('.card').cloneNode(true);
-	const cardImage = card.querySelector('.card__image');
-	const cardName = card.querySelector('.card__name');
-	cardImage.src = item.link;
-	cardImage.alt = item.name;
-	cardName.textContent = item.name;
-	setLikeListener(card);
-	setDeleteListener(card);
-	cardImage.addEventListener('click', () => handleCardClick(item)) ;
-	return card;
+function createCard(item) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardName = card.querySelector('.card__name');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardName.textContent = item.name;
+    setLikeListener(card);
+    setDeleteListener(card);
+    cardImage.addEventListener('click', () => handleCardClick(item));
+    return card;
 }
 
-initialCards.forEach(function(item) {
-	const card = createCard(item);
-	cardsList.append(card);
-})
-
-function handleProfileFormSubmit (evt) {
-	evt.preventDefault(); 
-	profileName.textContent = nameInput.value; 
-	profileJob.textContent = jobInput.value; 
-	closePopup(profilePopup); 
-}
-editBtn.addEventListener('click', () => { 
-	openPopup(profilePopup);
-	nameInput.value = profileName.textContent;
-	jobInput.value = profileJob.textContent;
+initialCards.forEach(function (item) {
+    const card = createCard(item);
+    cardsList.append(card);
 });
- 
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closePopup(profilePopup);
+}
+editBtn.addEventListener('click', () => {
+    openPopup(profilePopup);
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+});
+
 editForm.addEventListener('submit', handleProfileFormSubmit);
- 
-function handleAddFormSubmit (evt) {
-	evt.preventDefault();
-	const card = createCard({link: cardLink.value, name: cardName.value});
-	cardsList.prepend(card);
-	evt.target.reset();
-	closePopup(cardPopup);
+
+function handleAddFormSubmit(evt) {
+    evt.preventDefault();
+    const card = createCard({ link: cardLink.value, name: cardName.value });
+    cardsList.prepend(card);
+    evt.target.reset();
+    closePopup(cardPopup);
+    evt.submitter.classList.add('form__submit-btn_inactive');
+    evt.submitter.disabled = true;
 }
 
 addBtn.addEventListener('click', () => openPopup(cardPopup));
--- scripts/validate.js
+++ scripts/validate.js
@@ -4,60 +4,55 @@ const validationConfig = {
     submitButtonSelector: '.form__submit-btn',
     inactiveButtonClass: 'form__submit-btn_inactive',
     inputErrorClass: 'form__item-error',
-    errorClass: 'form__item_invalid'
-  }; 
+    errorClass: 'form__item_invalid',
+};
 
-const enableValidation = ({formSelector, ...rest}) => {
-    const forms = Array.from(document.querySelectorAll(formSelector))
-    forms.forEach(form => {
-        form.addEventListener('submit', (evt) => {
-            evt.preventDefault()
-        })
-        setEventListeners(form, rest)
-    })
-}
+const enableValidation = ({ formSelector, ...rest }) => {
+    const forms = Array.from(document.querySelectorAll(formSelector));
+    forms.forEach((form) => {
+        setEventListeners(form, rest);
+    });
+};
 
 const setEventListeners = (formToValidate, { inputSelector, submitButtonSelector, errorClass, ...rest }) => {
-    const formsInputs = Array.from(formToValidate.querySelectorAll(inputSelector))
-    const formButton = formToValidate.querySelector(submitButtonSelector)
-    disableButton(formButton, rest)
-    formsInputs.forEach(input => {
+    const formsInputs = Array.from(formToValidate.querySelectorAll(inputSelector));
+    const formButton = formToValidate.querySelector(submitButtonSelector);
+    disableButton(formButton, rest);
+    formsInputs.forEach((input) => {
         input.addEventListener('input', () => {
-            checkInputValidity(input, errorClass)
+            checkInputValidity(input, errorClass);
             if (hasInvalidInput(formsInputs)) {
-                disableButton(formButton, rest)
+                disableButton(formButton, rest);
             } else {
-                enableButton(formButton, rest)
+                enableButton(formButton, rest);
             }
-        })
-    })
-}
+        });
+    });
+};
 
 const hasInvalidInput = (formsInputs) => {
-    return !formsInputs.every((value) => value.checkValidity())
-} 
+    return !formsInputs.every((value) => value.checkValidity());
+};
 
 const checkInputValidity = (input, errorClass) => {
-    const currentInputErrorContainer = document.querySelector(`#${input.id}-error`)
+    const currentInputErrorContainer = document.querySelector(`#${input.id}-error`);
     if (input.checkValidity()) {
-        currentInputErrorContainer.textContent = ''
-        input.classList.remove(errorClass)
+        currentInputErrorContainer.textContent = '';
+        input.classList.remove(errorClass);
     } else {
-        currentInputErrorContainer.textContent = input.validationMessage
-        input.classList.add(errorClass)
+        currentInputErrorContainer.textContent = input.validationMessage;
+        input.classList.add(errorClass);
     }
-}
+};
 
-const enableButton = (button, { submitButtonSelector, inactiveButtonClass, ...rest}) =>{
-    button.classList.remove(inactiveButtonClass)
-    button.classList.add(submitButtonSelector)
-    button.addAttribute('disabled', true)
-}
+const enableButton = (button, {inactiveButtonClass }) => {
+    button.classList.remove(inactiveButtonClass);
+    button.removeAttribute('disabled');
+};
 
-const disableButton = (button, { submitButtonSelector, inactiveButtonClass, ...rest}) => {
-    button.classList.add(inactiveButtonClass)
-    button.classList.remove(submitButtonSelector)
-    button.removeAttribute('disabled')
-}
+const disableButton = (button, {inactiveButtonClass }) => {
+    button.classList.add(inactiveButtonClass);
+    button.setAttribute('disabled', true);
+};
 
-enableValidation(validationConfig)
+enableValidation(validationConfig);
